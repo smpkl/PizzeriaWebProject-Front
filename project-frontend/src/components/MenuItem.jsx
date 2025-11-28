b80f8911dc5a13935b686dcf67d35d4241b3f09e
riikka <riikaka@metropolia.fi>
2025-11-28 21:32:31 +0200

    Started working on the main view of the site. Started MenuItem component. Got items from the database to appear on the menu!

---
+++ project-frontend/src/components/MenuItem.jsx
@@ -0,0 +1,31 @@
+// Pizzerian info sivu (esittely, kartta, lomake, yhteystiedot)
+const MenuItem = ({item}) => {
+  return (
+    <>
+      <div
+        className="menu-item-card"
+        style={{display: 'flex', width: '90%', margin: 'auto'}}
+      >
+        <div
+          className="menu-item-img-container"
+          style={{display: 'flex', justifyItems: 'center'}}
+        >
+          <img
+            src="https://placehold.co/120x120/green/white?text=PRODUCT"
+            alt="A menu item image"
+            style={{margin: 'auto'}}
+          />
+        </div>
+        <div className="menu-item-info">
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+          <p>{item.tags ?? 'No tags yet'}</p>
+          <p>{item.price}</p>
+          <button>Add to order</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MenuItem;
--- project-frontend/src/views/general/Home.jsx
+++ project-frontend/src/views/general/Home.jsx
@@ -1,9 +1,37 @@
 // Päänäkymä/Home/Main
 
+import MenuFilter from '../../components/MenuFilter';
+import MenuItem from '../../components/MenuItem';
+import {useProducts} from '../../hooks/apiHooks';
+
 const Home = () => {
+  const {menuItems} = useProducts();
+
   return (
     <>
       <h1>MAIN</h1>
+      <div id="main-page-img-container">
+        <img
+          src="https://placehold.co/400x200/red/white?text=THIS+IS+A+PIZZA"
+          alt="A very delicious pizza"
+        />
+      </div>
+      <div id="start-order-box"></div>
+      <div id="announcement"></div>
+      <div id="meal-of-the-day"></div>
+      <div id="menu-container">
+        <h2>MENU</h2>
+        <MenuFilter />
+        <div id="menu">
+          {menuItems.map((item) => (
+            <MenuItem
+              key={item.id}
+              item={item}
+              //setSelectedItem={setSelectedItem}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
