1a740d7882fb17df4fae3c042b350fa2985ffc21
Elias Rinne <elias1rinne@gmail.com>
2025-11-29 17:55:01 +0200

    add order view for admin page

--- project-frontend/src/views/admin/AdminOrders.jsx
+++ project-frontend/src/views/admin/AdminOrders.jsx
@@ -1,1 +1,38 @@
-// Adminsivun tilausten hallinta (+ mahdollisesti käyttäjähaku/-hallinta?)
+import React from 'react';
+import OrderCard from '../../components/OrderCard';
+import { mockOrderList } from '../../mock-data/ordersMockData';
+
+const AdminOrders = () => {
+//mock dataa testaamista varten, tuossa muodossa pitäis saada tulee apista
+const orders = mockOrderList;
+
+  //Laitan jo 2 kieli optiota joka hallitaa tod näk shared contextilla
+  const langEn = true;
+  const langFin = false;
+  const styleContainer = {'display': 'flex', 'justifyContent': 'center'}
+  const styleOrders = {'border': '5px solid black', 'margin': '1rem', 'padding': '0.5rem'}
+  return (
+    <div style={styleContainer}>
+      <div style={styleOrders}>
+        {(langEn && <h2>New orders</h2>) ||
+          (langFin && <h2>Uudet tilaukset</h2>)}
+        {orders
+          .filter((order) => order.status === 'new')
+          .map((order) => (
+            <OrderCard key={order.id} orderInfo={order} />
+          ))}
+      </div>
+      <div style={styleOrders}>
+        {(langEn && <h2>Active orders</h2>) ||
+          (langFin && <h2>Aktiiviset tilaukset</h2>)}
+        {orders
+          .filter((order) => order.status === 'pending')
+          .map((order) => (
+            <OrderCard key={order.id} orderInfo={order} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default AdminOrders;
