cc8458547784d22ce201653249a3f01a25cd315f
Elias Rinne <elias1rinne@gmail.com>
2025-11-30 10:01:16 +0200

    add product card with styling. No api calls yet

--- project-frontend/src/views/admin/AdminProducts.jsx
+++ project-frontend/src/views/admin/AdminProducts.jsx
@@ -1,1 +1,39 @@
-// Adminsivun tuotteidenhallinta (+ mahdollisesti myös tagien- ja kategorioidenhallinta?)
+import React from 'react';
+import ProductCard from '../../components/ProductCard';
+
+const AdminProducts = () => {
+  //Laitan jo 2 kieli optiota joka hallitaa tod näk shared contextilla
+  const langEn = false;
+  const langFin = true;
+  return (
+    <div>
+      {langFin && (
+        <div>
+          <button>Lisää tuote</button>
+          <label htmlFor="product">Etsi tuote: </label>
+          <input
+            type="text"
+            id="product"
+            name="product"
+            placeholder="syötä nimi"
+          />
+        </div>
+      )}
+      {langEn && (
+        <div>
+          <button>Add product</button>
+          <label htmlFor="product">Search for a product: </label>
+          <input
+            type="text"
+            id="product"
+            name="product"
+            placeholder="syötä nimi"
+          />
+        </div>
+      )}
+      <ProductCard />
+    </div>
+  );
+};
+
+export default AdminProducts;
