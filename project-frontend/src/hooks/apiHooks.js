0f2ec506837387d76a8891d020dd306521832e30
Elias Rinne <elias1rinne@gmail.com>
2025-12-02 17:28:05 +0200

    add api hooks for posting new product with image

--- project-frontend/src/hooks/apiHooks.js
+++ project-frontend/src/hooks/apiHooks.js
@@ -1,7 +1,11 @@
 import {useState, useEffect} from 'react';
 import fetchData from '../utils/fetchData';
 
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo3NiwiZmlyc3RuYW1lIjoiTmV3MSIsImxhc3RuYW1lIjoiQWRtaW4yIiwiZW1haWwiOiJuZXdhZG1pbisxMTE3NjQ2NjkzNjI4NzZAZXhhbXBsZS5jb20iLCJhZGRyZXNzIjoiQWRtaW4gc3RyZWV0IDIsIEhlbHNpbmtpIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzY0NjcwMTQ3LCJleHAiOjE3NjQ3NTY1NDd9.wvVur03Z_OTxka0gXGGUVtBqQT00v4AppoAu_W0ojD4';
+
 const useProducts = () => {
+  const productUrl = 'http://127.0.0.1:3000/api/v1/products';
   const getProducts = async () => {
     try {
       const productData = await fetchData(
@@ -14,22 +18,59 @@ const useProducts = () => {
     }
   };
 
-  const postProduct = async (name, price, tags, category, ingredients, description, image) => {
-    body = {
+  const postProduct = async (inputs, checkbox, image) => {
+    const {price, name, category, ingredients, description} = inputs;
+
+  const formData = new FormData();
+  formData.append('name', name);
+  formData.append('price', parseFloat(price));
+  formData.append('category', parseInt(category));
+  formData.append('ingredients', ingredients);
+  formData.append('description', description);
+
+  // jos haluat tagit mukaan ja backend tukee niitä:
+  if (Array.isArray(checkbox)) {
+    checkbox.forEach((tag) => formData.append('tags[]', tag));
+  }
+
+  if (image) {
+    formData.append('file', image);               // tämä vastaa upload.single("file")
+  }
+
+    const postBody = {
       name: name,
       price: price,
-      tags: tags,
-      category: category,
+      tags: checkbox,
+      category: parseInt(category),
       ingredients: ingredients,
-      description: description
+      description: description,
+      file: formData
+    };
+
+
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    };
+
+    try {
+      console.log(productUrl);
+      const postProductData = await fetchData(productUrl, options);
+      if (postProductData) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-  }
+  };
 
-  const postImage = async (image) => {
-
-  }
-
-  return {getProducts, postProduct, postImage};
+  return {getProducts, postProduct};
 };
 
 const useAnnouncements = () => {
