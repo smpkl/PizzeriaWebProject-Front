67f1eeaa247bdd3e2e4e8ff9600c447e9a5eb59f
riikka <riikaka@metropolia.fi>
2025-12-02 11:04:40 +0200

    Cleaned the order forms' layout.

--- project-frontend/src/components/AtPizzeriaForm.jsx
+++ project-frontend/src/components/AtPizzeriaForm.jsx
@@ -9,120 +9,134 @@ const AtPizzeriaForm = () => {
     <>
       <div style={{border: '1px solid black'}}>
         <h3>AT PIZZERIA</h3>
-        <div
-          action=""
-          id="pizzeria-location-selection"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '20px 0',
-          }}
-        >
-          <label htmlFor="search-pizzeria">Search pizzeria: </label>
-          <input
-            type="text"
-            name="search-pizzeria"
-            id="search-pizzeria"
-            style={{margin: 'auto'}}
-            placeholder="Search for a pizzeria with address"
-          ></input>
-          <div>
-            {pizzerias.map((l) => (
-              <p>
-                {l.name} - {l.address}
-              </p>
-            ))}
-          </div>
-        </div>
         <form
           action=""
-          id="atPizzeria-time-form"
+          id="atPizzeria-form"
           style={{
             display: 'flex',
             flexDirection: 'column',
-            backgroundColor: 'lightyellow',
             padding: '20px 0',
           }}
         >
-          <h4>Arrival time: </h4>
-          <label htmlFor="atPizzeriap-time-option1">PREORDER</label>
-          <input
-            type="radio"
-            name="atPizzeria-time-option"
-            id="atPizzeria-time-option1"
-            value="preorder"
-          ></input>
-          <label htmlFor="atPizzeria-time-option2">NOW</label>
-          <input
-            type="radio"
-            name="atPizzeria-time-option"
-            id="atPizzeria-time-option2"
-            value="now"
-          ></input>
-          {timeOption === 'preorder' && (
+          <div
+            action=""
+            id="atPizzeria-location-container"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: '20px 0',
+            }}
+          >
+            <label htmlFor="atPizzeria-search-pizzeria">
+              Search pizzeria:{' '}
+            </label>
+            <input
+              type="text"
+              name="atPizzeria-search-pizzeria"
+              id="atPizzeria-search-pizzeria"
+              style={{margin: 'auto'}}
+              placeholder="Search for a pizzeria with address"
+            ></input>
             <div>
-              <label htmlFor="day-input">DATE: </label>
-              <input type="date" name="day-input" id="day-input"></input>
-              <label htmlFor="time-input">TIME: </label>
-              <input type="time" name="time-input" id="time-input"></input>
+              {pizzerias.map((l) => (
+                <p>
+                  {l.name} - {l.address}
+                </p>
+              ))}
             </div>
-          )}
-        </form>
-        <form
-          action=""
-          id="atPizzeria-user-info-form"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '20px 0',
-          }}
-        >
-          <h4>Contact information: </h4>
-          <label htmlFor="firstname-lastname">FIRSTNAME & LASTNAME: </label>
-          <input
-            type="text"
-            name="firstname-lastname"
-            id="firstname-lastname"
-            style={{margin: 'auto'}}
-            placeholder="Firstname Lastname"
-          ></input>
-          <label htmlFor="phonenumber">PHONENUMBER: </label>
-          <input
-            type="text"
-            name="phonenumber"
-            id="phonenumber"
-            style={{margin: 'auto'}}
-            placeholder="e.g. 050 000 000 00"
-          ></input>
-          <label htmlFor="email">EMAIL: </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            style={{margin: 'auto'}}
-            placeholder="Email address"
-          ></input>
-        </form>
-        <form
-          action=""
-          id="atPizzeria-details-form"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            backgroundColor: 'lightgrey',
-            padding: '20px 0',
-          }}
-        >
-          <h4>Order details: </h4>
-          <label htmlFor="atPizzeria-details">DETAILS: </label>
-          <textarea
-            rows="10"
-            cols="45"
-            name="atPizzeria-details"
-            id="atPizzeria-details"
-            placeholder="Type details for the pizzeria here"
-            style={{margin: 'auto'}}
-          ></textarea>
+          </div>
+          <div
+            id="atPizzeria-time-container"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              border: '1px solid black',
+            }}
+          >
+            {' '}
+            <h4>Arrival time: </h4>
+            <label htmlFor="atPizzeria-time-option1">PREORDER</label>
+            <input
+              type="radio"
+              name="atPizzeria-time-option"
+              id="atPizzeria-time-option1"
+              value="preorder"
+            ></input>
+            <label htmlFor="atPizzeria-time-option2">NOW</label>
+            <input
+              type="radio"
+              name="atPizzeria-time-option"
+              id="atPizzeria-time-option2"
+              value="now"
+            ></input>
+            {timeOption === 'preorder' && (
+              <div>
+                <label htmlFor="atPizzeria-day-input">DATE: </label>
+                <input
+                  type="date"
+                  name="atPizzeria-day-input"
+                  id="atPizzeria-day-input"
+                ></input>
+                <label htmlFor="atPizzeria-time-input">TIME: </label>
+                <input
+                  type="time"
+                  name="atPizzeria-time-input"
+                  id="atPizzeria-time-input"
+                ></input>
+              </div>
+            )}
+          </div>
+          <div
+            id="atPizzeria-contact-information-container"
+            style={{
+              backgroundColor: 'gray',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <h4>Contact information: </h4>
+            <label htmlFor="atPizzeria-firstname-lastname">
+              FIRSTNAME & LASTNAME:{' '}
+            </label>
+            <input
+              type="text"
+              name="atPizzeria-firstname-lastname"
+              id="atPizzeria-firstname-lastname"
+              style={{margin: 'auto'}}
+              placeholder="Firstname Lastname"
+            ></input>
+            <label htmlFor="atPizzeria-phonenumber">PHONENUMBER: </label>
+            <input
+              type="text"
+              name="atPizzeria-phonenumber"
+              id="atPizzeria-phonenumber"
+              style={{margin: 'auto'}}
+              placeholder="e.g. 050 000 000 00"
+            ></input>
+            <label htmlFor="atPizzeria-email">EMAIL: </label>
+            <input
+              type="text"
+              name="atPizzeria-email"
+              id="atPizzeria-email"
+              style={{margin: 'auto'}}
+              placeholder="Email address"
+            ></input>
+          </div>
+          <div
+            id="atPizzeria-details-container"
+            style={{display: 'flex', flexDirection: 'column'}}
+          >
+            <h4>Order details: </h4>
+            <label htmlFor="atPizzeria-details">DETAILS: </label>
+            <textarea
+              rows="10"
+              cols="45"
+              name="atPizzeria-details"
+              id="atPizzeria-details"
+              placeholder="Type details for the pizzeria here"
+              style={{margin: 'auto'}}
+            ></textarea>
+          </div>
         </form>
         <button>TO CHECKOUT</button>
       </div>
--- project-frontend/src/components/PickUpForm.jsx
+++ project-frontend/src/components/PickUpForm.jsx
@@ -9,120 +9,132 @@ const PickUpForm = () => {
     <>
       <div style={{border: '1px solid black'}}>
         <h3>PICK UP</h3>
-        <div
-          action=""
-          id="pizzeria-location-selection"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '20px 0',
-          }}
-        >
-          <label htmlFor="search-pizzeria">Search pizzeria: </label>
-          <input
-            type="text"
-            name="search-pizzeria"
-            id="search-pizzeria"
-            style={{margin: 'auto'}}
-            placeholder="Search for a pizzeria with address"
-          ></input>
-          <div>
-            {pizzerias.map((l) => (
-              <p>
-                {l.name} - {l.address}
-              </p>
-            ))}
-          </div>
-        </div>
         <form
           action=""
-          id="pickup-time-form"
+          id="pickup-form"
           style={{
             display: 'flex',
             flexDirection: 'column',
-            backgroundColor: 'lightyellow',
             padding: '20px 0',
           }}
         >
-          <h4>Pick up time: </h4>
-          <label htmlFor="pickup-time-option1">PREORDER</label>
-          <input
-            type="radio"
-            name="pickup-time-option"
-            id="pickup-time-option1"
-            value="preorder"
-          ></input>
-          <label htmlFor="pickup-time-option2">NOW</label>
-          <input
-            type="radio"
-            name="pickup-time-option"
-            id="pickup-time-option2"
-            value="now"
-          ></input>
-          {timeOption === 'preorder' && (
+          <div
+            action=""
+            id="pickup-location-container"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: '20px 0',
+            }}
+          >
+            <label htmlFor="pickup-search-pizzeria">Search pizzeria: </label>
+            <input
+              type="text"
+              name="pickup-search-pizzeria"
+              id="pickup-search-pizzeria"
+              style={{margin: 'auto'}}
+              placeholder="Search for a pizzeria with address"
+            ></input>
             <div>
-              <label htmlFor="day-input">DATE: </label>
-              <input type="date" name="day-input" id="day-input"></input>
-              <label htmlFor="time-input">TIME: </label>
-              <input type="time" name="time-input" id="time-input"></input>
+              {pizzerias.map((l) => (
+                <p>
+                  {l.name} - {l.address}
+                </p>
+              ))}
             </div>
-          )}
-        </form>
-        <form
-          action=""
-          id="pickup-user-info-form"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '20px 0',
-          }}
-        >
-          <h4>Contact information: </h4>
-          <label htmlFor="firstname-lastname">FIRSTNAME & LASTNAME: </label>
-          <input
-            type="text"
-            name="firstname-lastname"
-            id="firstname-lastname"
-            style={{margin: 'auto'}}
-            placeholder="Firstname Lastname"
-          ></input>
-          <label htmlFor="phonenumber">PHONENUMBER: </label>
-          <input
-            type="text"
-            name="phonenumber"
-            id="phonenumber"
-            style={{margin: 'auto'}}
-            placeholder="e.g. 050 000 000 00"
-          ></input>
-          <label htmlFor="email">EMAIL: </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            style={{margin: 'auto'}}
-            placeholder="Email address"
-          ></input>
-        </form>
-        <form
-          action=""
-          id="pickup-details-form"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            backgroundColor: 'lightgrey',
-            padding: '20px 0',
-          }}
-        >
-          <h4>Pick up details: </h4>
-          <label htmlFor="pickup-details">DETAILS: </label>
-          <textarea
-            rows="10"
-            cols="45"
-            name="pickup-details"
-            id="pickup-details"
-            placeholder="Type details for the pizzeria here"
-            style={{margin: 'auto'}}
-          ></textarea>
+          </div>
+          <div
+            id="pick-time-container"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              border: '1px solid black',
+            }}
+          >
+            {' '}
+            <h4>Pickup time: </h4>
+            <label htmlFor="pickup-time-option1">PREORDER</label>
+            <input
+              type="radio"
+              name="pickup-time-option"
+              id="pickup-time-option1"
+              value="preorder"
+            ></input>
+            <label htmlFor="pickup-time-option2">NOW</label>
+            <input
+              type="radio"
+              name="pickup-time-option"
+              id="pickup-time-option2"
+              value="now"
+            ></input>
+            {timeOption === 'preorder' && (
+              <div>
+                <label htmlFor="pickup-day-input">DATE: </label>
+                <input
+                  type="date"
+                  name="pickup-day-input"
+                  id="pickup-day-input"
+                ></input>
+                <label htmlFor="pickup-time-input">TIME: </label>
+                <input
+                  type="time"
+                  name="pickup-time-input"
+                  id="pickup-time-input"
+                ></input>
+              </div>
+            )}
+          </div>
+          <div
+            id="pickup-contact-information-container"
+            style={{
+              backgroundColor: 'gray',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <h4>Contact information: </h4>
+            <label htmlFor="pickup-firstname-lastname">
+              FIRSTNAME & LASTNAME:{' '}
+            </label>
+            <input
+              type="text"
+              name="pickup-firstname-lastname"
+              id="pickup-firstname-lastname"
+              style={{margin: 'auto'}}
+              placeholder="Firstname Lastname"
+            ></input>
+            <label htmlFor="pickup-phonenumber">PHONENUMBER: </label>
+            <input
+              type="text"
+              name="pickup-phonenumber"
+              id="pickup-phonenumber"
+              style={{margin: 'auto'}}
+              placeholder="e.g. 050 000 000 00"
+            ></input>
+            <label htmlFor="pickup-email">EMAIL: </label>
+            <input
+              type="text"
+              name="pickup-email"
+              id="pickup-email"
+              style={{margin: 'auto'}}
+              placeholder="Email address"
+            ></input>
+          </div>
+          <div
+            id="pickup-details-container"
+            style={{display: 'flex', flexDirection: 'column'}}
+          >
+            <h4>Pick up details: </h4>
+            <label htmlFor="pickup-details">DETAILS: </label>
+            <textarea
+              rows="10"
+              cols="45"
+              name="pickup-details"
+              id="pickup-details"
+              placeholder="Type details for the pizzeria here"
+              style={{margin: 'auto'}}
+            ></textarea>
+          </div>
         </form>
         <button>TO CHECKOUT</button>
       </div>
