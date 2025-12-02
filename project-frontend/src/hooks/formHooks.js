f7ea3738c57b47e37d9d2d8f746bd3141ed06b98
Elias Rinne <elias1rinne@gmail.com>
2025-12-02 17:28:36 +0200

    add formHooks for easier form handling

--- project-frontend/src/hooks/formHooks.js
+++ project-frontend/src/hooks/formHooks.js
@@ -1,35 +1,46 @@
 import {useState} from 'react';
 
-const useForm = (callback, initState) => {
+const useForm = (callback, callback2, initState, initStateCheckbox) => {
   const [inputs, setInputs] = useState(initState);
-  const [checkbox, setCheckbox] = useState(initState);
+  const [checkbox, setCheckbox] = useState(initStateCheckbox);
 
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
     }
-    callback(inputs);
+    callback(inputs, checkbox);
   };
 
+  //decides type by elements type attribute.
   const handleInputChange = (event) => {
     event.persist();
     setInputs((inputs) => ({
       ...inputs,
-      [event.target.name]: event.target.value,
+      [event.target.name]:
+        event.target.type === 'number'
+          ? event.target.value === ''
+            ? ''
+            : Number(event.target.value)
+          : event.target.value,
     }));
   };
 
+  //saves checkbox values as a list which are checked (checked === true)
   const handleCheckBox = (event) => {
     event.persist();
     const {value, checked} = event.target;
     setCheckbox((prev) => {
-      if (checked) {
-        if (prev.includes(value)) {
-          return prev;
+      if (prev) {
+        if (checked) {
+          if (prev.includes(value)) {
+            return prev;
+          }
+          return [...prev, value];
+        } else {
+          return prev.filter((tag) => tag !== value);
         }
-        return [...prev, value];
       } else {
-        return prev.filter((tag) => tag !== value);
+        return [value];
       }
     });
   };
