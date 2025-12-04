88dc9d35193280ef9ab665b0a0293a10ee5382e8
Elias Rinne <elias1rinne@gmail.com>
2025-12-04 15:47:34 +0200

    delete dual language option

--- project-frontend/src/components/OrderCard.jsx
+++ project-frontend/src/components/OrderCard.jsx
@@ -10,52 +10,30 @@ const OrderCard = (order) => {
 
   const products = order.orderInfo.products;
 
-
-  //Laitan jo 2 kieli optiota joka hallitaa tod näk shared contextilla
-  const langEn = false;
-  const langFin = true;
-
-  const styleOrderCard = {'border': '1px solid grey', 'margin': '0.5rem', 'padding': '0.2rem'}
+  const styleOrderCard = {
+    border: '1px solid grey',
+    margin: '0.5rem',
+    padding: '0.2rem',
+  };
   return (
     <div style={styleOrderCard}>
-      {(langEn && (
-        <>
-          <h3>Orders information</h3>
-          {products.map((product) => (
-            <OrderProductCard
-              key={product.id}
-              product={product}
-              orderType={orderType}
-              deliveryAddress={deliveryAddress}
-            />
-          ))}
-          {(status === 'new' && (
-            <>
-              <button>Accept</button> <button>Cancel</button>
-            </>
-          )) ||
-            (status === 'pending' && <button>Completed</button>)}
-        </>
-      )) ||
-        (langFin && (
-          <>
-            <h3>Tilauksen tiedot</h3>
-            {products.map((product) => (
-              <OrderProductCard
-                key={product.id}
-                product={product}
-                orderType={orderType}
-                deliveryAddress={deliveryAddress}
-              />
-            ))}
-            {(status === 'new' && (
-              <>
-                <button>Hyväksy</button> <button>peruuta</button>
-              </>
-            )) ||
-              (status === 'pending' && <button>Valmis</button>)}
-          </>
+      <>
+        <h3>Orders information</h3>
+        {products.map((product) => (
+          <OrderProductCard
+            key={product.id}
+            product={product}
+            orderType={orderType}
+            deliveryAddress={deliveryAddress}
+          />
         ))}
+        {(status === 'new' && (
+          <>
+            <button>Accept</button> <button>Cancel</button>
+          </>
+        )) ||
+          (status === 'pending' && <button>Completed</button>)}
+      </>
     </div>
   );
 };
