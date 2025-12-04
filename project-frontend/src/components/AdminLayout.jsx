2139053af3a6267e607c757a2c679caf6e69c628
Elias Rinne <elias1rinne@gmail.com>
2025-12-04 15:47:11 +0200

    add routes on admin layout and add select for product management etc as in figma

--- project-frontend/src/components/AdminLayout.jsx
+++ project-frontend/src/components/AdminLayout.jsx
@@ -1,32 +1,58 @@
-import {Outlet, Link} from 'react-router';
+import {Outlet, Link, useNavigate} from 'react-router';
 
-const Layout = () => (
-  <div>
-    <header style={{display: 'flex', backgroundColor: 'lightgray'}}>
-      <div style={{width: '30%'}}>
-        <img
-          src="https://placehold.co/100x100/orange/white?text=LOGO"
-          alt="Pizzeria Logo goes here"
-        />
-      </div>
-      <nav>
-        <ul style={{display: 'flex', margin: '0', padding: '0'}}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/admin/orders">admin</Link>
-          </li>
-          <li>
-            <Link to="/admin/products">Add products</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <main>
-      <Outlet />
-    </main>
-  </div>
-);
+const Layout = () => {
+  const navigate = useNavigate();
 
+  const handleAdminSelect = (evt) => {
+    const path = evt.target.value;
+    if (path) {
+      navigate(path);
+    }
+  };
+  return (
+    <div>
+      <header style={{display: 'flex', backgroundColor: 'lightgray'}}>
+        <div style={{width: '30%'}}>
+          <img
+            src="https://placehold.co/100x100/orange/white?text=LOGO"
+            alt="Pizzeria Logo goes here"
+          />
+        </div>
+        <nav>
+          <ul style={{display: 'flex', margin: '0', padding: '0'}}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin/login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/admin/orders">Orders</Link>
+            </li>
+            <li>
+              <select defaultValue="" onChange={handleAdminSelect}>
+                <option value="" disabled>
+                  Admin pages
+                </option>
+                <option value="/admin/products">Products</option>
+                <option value="/admin/announcements">Announcements</option>
+                <option value="/admin/coupons">Coupons</option>
+              </select>
+            </li>
+            {/* Odottaa että valmistuu ja linkitetää projektii */}
+            <li>
+              <Link to="/admin/feedbacks">Feedbacks</Link>
+            </li>
+            <li>
+              <Link to="/admin/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 export default Layout;
