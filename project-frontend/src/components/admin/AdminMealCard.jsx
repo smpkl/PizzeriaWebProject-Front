d5720e20f437b2baef5838c6a9813d237e2d4705
Elias Rinne <elias1rinne@gmail.com>
2025-12-05 21:13:57 +0200

    add meal card using base from other component

---
+++ project-frontend/src/components/admin/AdminMealCard.jsx
@@ -0,0 +1,102 @@
+import {useState} from 'react';
+import ItemDialog from '../ItemDialog';
+
+const AdminMealCard = ({item, setModifyMeal, setShowModified}) => {
+  const [selectedProduct, setSelectedProduct] = useState();
+  const products = item.products;
+
+  const handleShowItemDetails = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleCloseItemDetails = () => {
+    setSelectedProduct(null);
+  };
+
+  //const handleModifyMeal = async();
+
+  return (
+    <>
+      <div
+        className="meal-item-card"
+        id={`meal-${item.id}`}
+        style={{
+          display: 'flex',
+          width: '90%',
+          margin: '5px auto',
+          padding: '2%',
+          backgroundColor: 'lightgray',
+        }}
+      >
+        <div
+          className="meal-item-img-container"
+          style={{display: 'flex', justifyItems: 'center'}}
+        >
+          <img
+            src={
+              item.filename
+                ? `http://127.0.0.1:3000/api/v1/uploads/${item.filename}`
+                : 'https://placehold.co/120x120/green/white?text=PRODUCT'
+            }
+            alt="A menu item image"
+            style={{margin: 'auto'}}
+          />
+        </div>
+        <div className="menu-item-info">
+          <h3>{item.name}</h3>
+          <h4>Products included in the meal:</h4>
+          <p>Click product to see details</p>
+          <ul className="meal-products-ul">
+            {products.map((product) => (
+              <li
+                key={`meal-product-${product.id}`}
+                className="products-in-meal"
+                onClick={() => handleShowItemDetails(product)}
+              >
+                {product.name}
+              </li>
+            ))}
+          </ul>
+          <div
+            style={{display: 'flex', justifyContent: 'right', width: '100%'}}
+          >
+            {item.oldPrice && (
+              <p
+                style={{
+                  textDecorationLine: 'line-through',
+                  color: 'darkred',
+                  marginRight: '10px',
+                }}
+              >
+                {item.oldPrice}€
+              </p>
+            )}
+            <p>{item.price}€</p>
+          </div>
+          <div
+            style={{display: 'flex', justifyContent: 'right', width: '100%'}}
+          >
+            <button
+              onClick={(evt) => {
+                evt.preventDefault;
+                setModifyMeal(item);
+                setShowModified(true);
+              }}
+            >
+              Modify
+            </button>
+          </div>
+        </div>
+        {selectedProduct && (
+          <ItemDialog
+            item={selectedProduct}
+            meal={item}
+            onClose={handleCloseItemDetails}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default AdminMealCard;
