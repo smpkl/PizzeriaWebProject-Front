a4c160938f590926775c5409f5ac12d091d84a0b
Elias Rinne <elias1rinne@gmail.com>
2025-12-11 16:27:21 +0200

    add main and login css and increase fontsize and fix bug on clipping on order card. Also increase buttonsizes

--- project-frontend/src/views/admin/AdminMain.jsx
+++ project-frontend/src/views/admin/AdminMain.jsx
@@ -1,25 +1,31 @@
 import {Navigate, useNavigate} from 'react-router';
 import {useAdminContext} from '../../hooks/contextHooks';
+import '../../admincss/admin.css';
 
 const AdminMain = () => {
   const {admin} = useAdminContext();
   const navigate = useNavigate();
 
   return (
-    <>
+    <div className="admin-main">
       {!admin && (
-        <div>
-          <h1>TBA PIZZERIA ADMIN SITE</h1>
-          <p>
-            Login as an admin by selecting "Login" from the navigation menu{' '}
-            <br />
-            or by clickin the button bellow.
+        <div className="admin-card admin-main__hero">
+          <h1 className="admin-main__title">TBA PIZZERIA ADMIN SITE</h1>
+          <p className="admin-main__text">
+            Login as an admin by selecting &quot;Login&quot; from the navigation
+            menu <br />
+            or by clicking the button below.
           </p>
-          <button onClick={() => navigate('/admin/login')}>Go to login</button>
+          <button
+            className="admin-button admin-main__button"
+            onClick={() => navigate('/admin/login')}
+          >
+            Go to login
+          </button>
         </div>
       )}
       {admin && <Navigate to={'/admin/orders'} />}
-    </>
+    </div>
   );
 };
 
